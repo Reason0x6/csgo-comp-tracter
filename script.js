459269157b6f38210f8af2b2fc383ec0bfe1b69e
Reason6 <37852683+Reason0x6@users.noreply.github.com>
2021-07-07 21:02:58 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
  var $table = $('#table')
 
-  $(function() {
-    var data = [httpGet("https://gist.githubusercontent.com/Reason0x6/8cda7cdce846941a4d9956606ad255e2/raw/46762ca4ea607a6a43ae7ccd95edf3ff16004d09/IEM_DAT")]
-    $table.bootstrapTable({data: data})
-  })
+ 
 
-
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+$.get(
+    "https://gist.githubusercontent.com/Reason0x6/8cda7cdce846941a4d9956606ad255e2/raw/46762ca4ea607a6a43ae7ccd95edf3ff16004d09/IEM_DAT",
+    {paramOne : 1, paramX : 'abc'},
+    function(db) {
+       $table.bootstrapTable({
+         columns: [{
+           field: 'id',
+           title: 'Game'
+         },
+           {
+           field: 'team1',
+           title: 'Team 1'
+         }, {
+           field: 'team2',
+           title: 'Team 2'
+         }],
+         data: [db]
+    }
+);
