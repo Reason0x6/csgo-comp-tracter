f2e4c6801a801af937531b580bbd44bd1274e47e
Reason6 <37852683+Reason0x6@users.noreply.github.com>
2021-07-07 22:53:26 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
  
 
 $.get(
-    "https://gist.githubusercontent.com/Reason0x6/8cda7cdce846941a4d9956606ad255e2/raw/46762ca4ea607a6a43ae7ccd95edf3ff16004d09/IEM_DAT",
-    {paramOne : 1, paramX : 'abc'},
+    "https://gist.githubusercontent.com/Reason0x6/8cda7cdce846941a4d9956606ad255e2/raw/3d5f3573e6408d018c33dde7b8d0705158a912fe/IEM_DAT",
+    
     function(db) {
        $table.bootstrapTable({
          columns: [{
