0b980f91437e48926f0d121b8795a726f83cda1f
Reason6 <37852683+Reason0x6@users.noreply.github.com>
2021-07-08 14:44:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
  var $table = $('#table');
 
- 
-
 $.get(
     "https://gist.githubusercontent.com/Reason0x6/8cda7cdce846941a4d9956606ad255e2/raw",
     
@@ -61,5 +59,21 @@ $.get(
    
     }
 );
-
+var valid = true;
+var i = 1;
+while(valid){
+ var data = null
+ var voting_url = "https://spreadsheets.google.com/feeds/cells/1notCppP0AXospf6d8bgxpZyjFw6ThDh1is-8rEjCxTQ/" + i + "/public/full?alt=json";
+ i++;
+ 
+ $.get(
+    "https://gist.githubusercontent.com/Reason0x6/2ef9ade8af06347df6cb7dc8c2708b90/raw",
+    
+    function(outcome) {
+       data = JSON.parse(outcome)
+    }
+);
+ console.log(data);
+ 
+}
 
