c5fe919b26545d947ca3b25186e54b76cc0150e1
Reason6 <37852683+Reason0x6@users.noreply.github.com>
2021-07-09 11:17:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -67,20 +67,63 @@ while(valid < 10){
  i++;
  
  data= $.get( voting_url, "json", function(outcome) {
+    var running = [];
     Feed = outcome.feed.entry;
     for(var i = 0; i < 100; i++){
-        if(Feed[i] != null && Feed[i].gs$cell.row != "1"){
+        
+        if(Feed[i] != null && (Feed[i].gs$cell.row != "1")){
+            if( Feed[i].gs$cell.inputValue == "noDataParse"){
+                return;
+            }
             if(Feed[i].gs$cell.inputValue == "Team 2"){
                 break;
             }
             if(Feed[i].gs$cell.col == "2" && !(Name.includes(Feed[i].gs$cell.inputValue))){
                 Name.push(Feed[i].gs$cell.inputValue);
+                running.push(Feed[i].gs$cell.inputValue);
+            }else{
+                if(!((Feed[i].gs$cell.inputValue).includes("/"))){
+                        running.push(Feed[i].gs$cell.inputValue);
+                }
+                
             }
         }
         
     }
+compile(Name,  running);
 });
  
  valid++;
 }
 
+
+
+function compile(x,y){
+    var currName = "";
+    var list = [];
+    var count = [];
+    var a = 0;
+    var z = [];
+    z = x;
+    var w = [];
+    w = y;
+
+
+    w.forEach(function(value){
+                    if(z.includes(value)){
+            
+            if(a != 0){
+                list.push(count);
+            }
+            count = [];
+            currName = value;
+            a++;
+            
+        }
+        else{
+            count.push(value);
+        }
+    });
+    console.log(list);
+    
+}
